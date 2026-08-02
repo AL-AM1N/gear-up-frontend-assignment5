2407a77d76c2d5570dc8754d8f77cfa770887722
MD. AL-AMIN <amin57.cse@gmail.com>
2026-08-02 19:44:19 +0600

    feat: add jwt utilities and auth services

---
+++ service/getMe.ts
@@ -0,0 +1,19 @@
+"use server";
+
+import { cookies } from "next/headers";
+
+export const getMe = async () => {
+  const cookieStore = await cookies();
+  const accessToken = cookieStore.get("accessToken")?.value || null;
+
+  if (!accessToken) {
+    return { success: false, message: "User not logged in!" };
+  }
+
+  const res = await fetch(`${process.env.BACKEND_API_URL}/api/auth/me`, {
+    headers: { Cookie: `accessToken=${accessToken}` },
+    cache: "no-store",
+  });
+
+  return res.json();
+};
